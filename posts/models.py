e7499edbd8ec5edd2701ca268a774caccfab1ba3
AlgoClassWork <kyxec.play@gmail.com>
2026-04-05 10:30:41 +0600

    Update models.py

--- posts/models.py
+++ posts/models.py
@@ -2,8 +2,11 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Post(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    title = models.CharField(max_length=10)
-    content = models.TextField()
-    image = models.ImageField(upload_to='posts/', null=True, blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Автор')
+    title = models.CharField(max_length=100, verbose_name='Заголовок')
+    content = models.TextField(verbose_name='Информация')
+    image = models.ImageField(upload_to='posts/', null=True, blank=True, verbose_name='Изображение')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+
+    def __str__(self):
+        return self.title
