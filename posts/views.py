82951c61640bd1d4f1cf8baed4d92b5222f9a126
AlgoClassWork <kyxec.play@gmail.com>
2026-04-12 11:30:36 +0600

    Add files via upload

--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,14 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.forms import UserCreationForm
 from .models import Post
 
 # Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        form.save()
+    return render(request, 'register.html', {'form' : form} )
+
 def post_list(request):
     posts = Post.objects.all()
     return render(request, 'index.html', {'posts': posts})
