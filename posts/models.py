77645e7653446a41a629b8f62eb7ae0b79310013
AlgoClassWork <kyxec.play@gmail.com>
2026-04-26 10:22:52 +0600

    Add related_name to Comment model's post field

--- posts/models.py
+++ posts/models.py
@@ -12,11 +12,11 @@ class Post(models.Model):
         return self.title
 
 class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, verbose_name='Пост')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, verbose_name='Пост', related_name='comments')
     author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Автор')
     content = models.TextField(verbose_name='Комментарий')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
 
     def __str__(self):
-        return self.title
+        return f'автор : {self.author} на пост : {self.post}'
 
